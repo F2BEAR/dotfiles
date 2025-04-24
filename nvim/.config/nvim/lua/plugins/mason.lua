f8413bdd0e51cab13e5e6b5ab5c71f8b8d610576
Facundo Carbonel <facundoc@takeoffmedia.com>
2025-04-24 19:19:35 -0300

    fix: mason config errors

--- nvim/.config/nvim/lua/plugins/mason.lua
+++ nvim/.config/nvim/lua/plugins/mason.lua
@@ -1,37 +1,21 @@
 return {
   {
     "williamboman/mason.nvim",
-    build = ":MasonUpdate",
-    cmd = "Mason",
-    opts = {
-      ui = {
-        border = "rounded",
-        icons = {
-          package_installed = "✓",
-          package_pending = "➜",
-          package_uninstalled = "✗",
-        },
-      },
-    },
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    dependencies = { "mason.nvim" },
     opts = {
       ensure_installed = {
         -- LSP servers
         "sqls",
-        "lua_ls", -- lua-language-server
-        "prismals", -- prisma-language-server
-        "yamlls", -- yaml-language-server
+        "lua-language-server",
+        "prisma-language-server",
+        "yaml-language-server",
         "marksman",
-        "eslint", -- eslint-lsp
-        "jsonls", -- json-lsp
-        "dockerls", -- dockerfile-language-server
-        "docker_compose_language_service",
-        "tailwindcss", -- tailwindcss-language-server
-        "vtsls", -- TypeScript/JavaScript server
-        "bashls", -- bash-language-server
+        "eslint-lsp",
+        "json-lsp",
+        "dockerfile-language-server",
+        "docker-compose-language-service",
+        "tailwindcss-language-server",
+        "vtsls",
+        "bash-language-server",
 
         -- Formatters
         "shfmt",
@@ -48,63 +32,57 @@ return {
     },
   },
   {
+    "williamboman/mason-lspconfig.nvim",
+    opts = {
+      ensure_installed = {
+        "sqls",
+        "lua_ls",
+        "prismals",
+        "yamlls",
+        "marksman",
+        "eslint",
+        "jsonls",
+        "dockerls",
+        "docker_compose_language_service",
+        "tailwindcss",
+        "vtsls",
+        "bashls",
+      },
+    },
+  },
+  {
     "nvimtools/none-ls.nvim",
     opts = function(_, opts)
       local null_ls = require("null-ls")
 
-      local function has_prettier_config(utils)
-        return utils.root_has_file({
-          ".prettierrc",
-          ".prettierrc.js",
-          ".prettierrc.json",
-          ".prettierrc.yml",
-          ".prettierrc.yaml",
-          ".prettierrc.toml",
-          ".prettierrc.json5",
-          "prettier.config.js",
-          "prettier.config.cjs",
-          ".prettierrc.cjs",
+      table.insert(
+        opts.sources or {},
+        null_ls.builtins.formatting.prettier.with({
+          prefer_local = "node_modules/.bin",
+          condition = function(utils)
+            return not utils.root_has_file({
+              ".prettierrc",
+              ".prettierrc.js",
+              ".prettierrc.json",
+              ".prettierrc.yml",
+              ".prettierrc.yaml",
+              ".prettierrc.toml",
+              ".prettierrc.json5",
+              "prettier.config.js",
+              "prettier.config.cjs",
+              ".prettierrc.cjs",
+            })
+          end,
+          extra_args = {
+            "--single-quote",
+            "--trailing-comma",
+            "all",
+            "--use-tabs",
+            "--tab-width",
+            "2",
+          },
         })
-      end
-
-      local prettier = null_ls.builtins.formatting.prettier.with({
-        condition = has_prettier_config,
-        dynamic_command = function(params)
-          if has_prettier_config(params.utils) then
-            return {
-              "prettier",
-              "--stdin-filepath",
-              params.bufname,
-            }
-          else
-            return {
-              "prettier",
-              "--stdin-filepath",
-              params.bufname,
-              "--single-quote",
-              "--trailing-comma",
-              "all",
-              "--use-tabs",
-              "--tab-width",
-              "2",
-            }
-          end
-        end,
-      })
-
-      opts.sources = opts.sources or {}
-      vim.list_extend(opts.sources, {
-        -- Formatting
-        prettier, -- Use our configured prettier
-        null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.shfmt,
-        null_ls.builtins.formatting.sql_formatter,
-
-        -- Linting
-        null_ls.builtins.diagnostics.shellcheck,
-        null_ls.builtins.diagnostics.hadolint,
-        null_ls.builtins.diagnostics.markdownlint,
-      })
+      )
 
       return opts
     end,
